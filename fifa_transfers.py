9640cffcfe2c7a151e471a987e0c28fed8d4542f
enadol <enadol@yahoo.com>
2024-09-05 17:39:08 +0200

    modified:   fifa_transfers.py

--- fifa_transfers.py
+++ fifa_transfers.py
@@ -3,6 +3,8 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 #read excel file "transfers_global_FIFA.xlsx" and assign it to a variable called df as DataFrame
 df = pd.read_excel("transfers_global_FIFA.xlsx")
 
@@ -35,20 +37,141 @@ transfers_received = group_and_sum(df, 'Transfers received')
 transfers_balance = group_and_sum(df, 'Transfers Balance')
 
 #print the results
-print("Fees received:\\n", transfers_received)
-print("Fees spent:\\n", transfers_spent)
+print("Fees received:", transfers_received)
+print("Fees spent:", transfers_spent)
+print("Transfers Balance:", transfers_balance)
 
+#make streamlit sidebar with a dropdown menu
+#options to choose between graphs:
+#"Fees spent", "Fees received", "Balance"
+#and a button to show the graph
+#the default option is "Balance"
+#the default button is "Show Graph"
+#the title of the sidebar is "Select a graph"
+#the title of the dropdown menu is "Graph type"
+#the title of the button is "Show Graph"
+#the title of the page is "FIFA TRANSFERS BALANCE BY CONFEDERATION (SUMMER 2024)"
+#the page should be wide
+with st.sidebar:
+    graph_type = st.selectbox("Graph type", ["Balance", "Fees spent", "Fees received"], index=0)
+    show_graph = st.button("Show Graph")
 
-#visualize the confederations balance with a bar chart
-#im plotly
-fig = px.bar(transfers_balance, x=transfers_balance.index,\
-y=transfers_balance.values, title="Transfers Balance by Confederation")
-#change the color of the bars to green if the value is positive and red if the value is negative
-fig.update_traces(marker_color=['green' if x >= 0 else 'red' for x in transfers_balance.values], \
-marker_line_color='rgb(8,48,107)', marker_line_width=1.5,\
-      opacity=0.6, selector=dict({'type':'bar'}))
-#change the size of the bars to 0.5
-fig.update_traces(width=0.5)
+st.title("FIFA TRANSFERS BALANCE BY CONFEDERATION (SUMMER 2024)")
 
-#show in streamlit
-st.plotly_chart(fig)
+#if the button is clicked, show the graph
+if show_graph:
+    if graph_type == "Balance":
+        #visualize the confederations balance with a bar chart
+        #im plotly. Change yaxis label to "Amount (USD)"
+        fig = px.bar(transfers_balance, x=transfers_balance.index, y=transfers_balance.values, \
+        labels={'x':'Confederation', 'y':'Amount (USD)'},\
+         title="TRANSFERS BALANCE BY CONFEDERATION (SUMMER 2024)", width=1000, height=600)
+        #change the color of the bars to green if the value
+        #is positive and red if the value is negative
+        fig.update_traces(marker_color=['green' if x >= 0 else \
+                    'red' for x in transfers_balance.values], \
+        marker_line_color='rgb(8,48,107)', marker_line_width=1.5,\
+              opacity=0.6, selector=dict({'type':'bar'}))
+        #change the size of the bars to 0.5
+        fig.update_layout(bargap=0.5)
+        #change the background color to white
+        fig.update_layout(plot_bgcolor='white')
+        #change the font color to black
+        fig.update_layout(font_color='black')
+        #change the title font size to 20
+        fig.update_layout(title_font_size=20)
+        #change the title font color to black
+        fig.update_layout(title_font_color='black')
+        #change the xaxis font size to 15
+        fig.update_layout(xaxis_tickfont_size=15)
+        #change the yaxis font size to 15
+        fig.update_layout(yaxis_tickfont_size=15)
+        #change the xaxis title font size to 20
+        fig.update_layout(xaxis_title_font_size=20)
+        #change the yaxis title font size to 20
+        fig.update_layout(yaxis_title_font_size=20)
+        #change the xaxis title font color to black
+        fig.update_layout(xaxis_title_font_color='black')
+        #change the yaxis title font color to black
+        fig.update_layout(yaxis_title_font_color='black')
+        #change the xaxis tick angle to 45 degrees
+        fig.update_layout(xaxis_tickangle=45)
+        #show the graph
+        st.plotly_chart(fig, use_container_width=True)
+    elif graph_type == "Fees spent":
+        #visualize the fees spent with a bar chart
+        #im plotly. Change yaxis label to "Amount (USD)
+        fig = px.bar(transfers_spent, x=transfers_spent.index, y=transfers_spent.values, \
+        labels={'x':'Confederation', 'y':'Amount (USD)'},\
+         title="FEES SPENT BY CONFEDERATION (SUMMER 2024)", width=1000, height=600)
+        #change the color of the bars to green if the value
+        #is positive and red if the value is negative
+        fig.update_traces(marker_color=['green' if x >= 0 else \
+                    'red' for x in transfers_spent.values], \
+        marker_line_color='rgb(8,48,107)', marker_line_width=1.5,\
+              opacity=0.6, selector=dict({'type':'bar'}))
+        #change the size of the bars to 0.5
+        fig.update_layout(bargap=0.5)
+        #change the background color to white
+        fig.update_layout(plot_bgcolor='white')
+        #change the font color to black
+        fig.update_layout(font_color='black')
+        #change the title font size to 20
+        fig.update_layout(title_font_size=20)
+        #change the title font color to black
+        fig.update_layout(title_font_color='black')
+        #change the xaxis font size to 15
+        fig.update_layout(xaxis_tickfont_size=15)
+        #change the yaxis font size to 15
+        fig.update_layout(yaxis_tickfont_size=15)
+        #change the xaxis title font size to 20
+        fig.update_layout(xaxis_title_font_size=20)
+        #change the yaxis title font size to 20
+        fig.update_layout(yaxis_title_font_size=20)
+        #change the xaxis title font color to black
+        fig.update_layout(xaxis_title_font_color='black')
+        #change the yaxis title font color to black
+        fig.update_layout(yaxis_title_font_color='black')
+        #change the xaxis tick angle to 45 degrees
+        fig.update_layout(xaxis_tickangle=45)
+        #show the graph
+        st.plotly_chart(fig, use_container_width=True)
+    else:
+        #graph for fees received
+        #visualize the fees received with a bar chart
+        #im plotly. Change yaxis label to "Amount (USD)
+        fig = px.bar(transfers_received, x=transfers_received.index, y=transfers_received.values, \
+                     labels={'x':'Confederation', 'y':'Amount (USD)'},\
+                     title="FEES RECEIVED BY CONFEDERATION (SUMMER 2024)", width=1000, height=600)
+        #change the color of the bars to green if the value
+        #is positive and red if the value is negative
+        fig.update_traces(marker_color=['green' if x >= 0 else \
+                    'red' for x in transfers_received.values], \
+        marker_line_color='rgb(8,48,107)', marker_line_width=1.5,\
+              opacity=0.6, selector=dict({'type':'bar'}))
+        #change the size of the bars to 0.5
+        fig.update_layout(bargap=0.5)
+        #change the background color to white
+        fig.update_layout(plot_bgcolor='white')
+        #change the font color to black
+        fig.update_layout(font_color='black')
+        #change the title font size to 20
+        fig.update_layout(title_font_size=20)
+        #change the title font color to black
+        fig.update_layout(title_font_color='black')
+        #change the xaxis font size to 15
+        fig.update_layout(xaxis_tickfont_size=15)
+        #change the yaxis font size to 15
+        fig.update_layout(yaxis_tickfont_size=15)
+        #change the xaxis title font size to 20
+        fig.update_layout(xaxis_title_font_size=20)
+        #change the yaxis title font size to 20
+        fig.update_layout(yaxis_title_font_size=20)
+        #change the xaxis title font color to black
+        fig.update_layout(xaxis_title_font_color='black')
+        #change the yaxis title font color to black
+        fig.update_layout(yaxis_title_font_color='black')
+        #change the xaxis tick angle to 45 degrees
+        fig.update_layout(xaxis_tickangle=45)
+        #show the graph
+        st.plotly_chart(fig, use_container_width=True)
