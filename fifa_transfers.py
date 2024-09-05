6dc89e719ba7c015e78bed2cb811fdd7eb1431cd
enadol <enadol@yahoo.com>
2024-09-05 17:09:05 +0200

    modified:   fifa_transfers.py 	modified:   transfers_global_FIFA.xlsx

--- fifa_transfers.py
+++ fifa_transfers.py
@@ -31,18 +31,20 @@ transfers_spent = group_and_sum(df, 'Transfers Spent')
 #use the function to group and sum the Transfers received column
 transfers_received = group_and_sum(df, 'Transfers received')
 
+#use the function to group and sum the Transfers Balance column
+transfers_balance = group_and_sum(df, 'Transfers Balance')
+
 #print the results
 print("Fees received:\\n", transfers_received)
 print("Fees spent:\\n", transfers_spent)
 
 
-#visualize the confederations results with a bar chart
+#visualize the confederations balance with a bar chart
 #im plotly
-fig = px.bar(transfers_spent, x=transfers_spent.index,\
-y=transfers_spent.values, title="Transfers Balance by Confederation")
+fig = px.bar(transfers_balance, x=transfers_balance.index,\
+y=transfers_balance.values, title="Transfers Balance by Confederation")
 #change the color of the bars to green if the value is positive and red if the value is negative
-#make values logarithmic
-fig.update_traces(marker_color=['green' if x >= 0 else 'red' for x in transfers_spent.values], \
+fig.update_traces(marker_color=['green' if x >= 0 else 'red' for x in transfers_balance.values], \
 marker_line_color='rgb(8,48,107)', marker_line_width=1.5,\
       opacity=0.6, selector=dict({'type':'bar'}))
 #change the size of the bars to 0.5
