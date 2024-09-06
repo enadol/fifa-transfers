574757988788d48a20c4a8f39e268ba0e8f12711
enadol <enadol@yahoo.com>
2024-09-06 20:55:42 +0200

    modified:   .vscode/settings.json 	modified:   fifa_transfers.py

--- fifa_transfers.py
+++ fifa_transfers.py
@@ -57,17 +57,19 @@ print("Transfers Balance:", transfers_balance)
 #the title of the page is "FIFA TRANSFERS BALANCE BY CONFEDERATION (SUMMER 2024)"
 #the page should be wide
 with st.sidebar:
-    graph_type = st.selectbox("Graph type", ["Balance", "Fees spent", "Fees received"], index=0)
-    show_graph = st.button("Show Graph")
+    graph_type = st.selectbox("Select analysis", ["Balance", "Fees spent", "Fees received"], index=0)
+    conf_type=st.selectbox(df['Confederation'].unique(), index=0)
+    show_conf_graph = st.button("Show Confederations Totals")
+    show_members_graph = st.button("Show Confederations per members")
 
-#if the button is clicked, show the graph
-if show_graph:
+#if the confederations button is clicked, show the graph for confederations
+if show_conf_graph:
     if graph_type == "Balance":
         #visualize the confederations balance with a bar chart
         #im plotly. Change yaxis label to "Amount (USD)"
         fig = px.bar(transfers_balance, x=transfers_balance.index, y=transfers_balance.values, \
         labels={'x':'Confederation', 'y':'Amount (USD)'},\
-         title="TRANSFERS BALANCE BY CONFEDERATION (SUMMER 2024)", width=1000, height=600)
+        title="TRANSFERS BALANCE BY CONFEDERATION (SUMMER 2024)", width=1000, height=600)
         #change the color of the bars to green if the value
         #is positive and red if the value is negative
         fig.update_traces(marker_color=['green' if x >= 0 else \
@@ -179,6 +181,51 @@ if show_graph:
         fig.update_layout(yaxis_title_font_color='black')
         #change the xaxis tick angle to 45 degrees
         fig.update_layout(xaxis_tickangle=45)
+
+    if show_members_graph:
+        df_filtered=df['Confederation'==show_members_graph]
+        #graph for fees received by member association
+        #visualize the fees received by member association with a bar chart
+        #im plotly. Change yaxis label to "Amount (USD)
+        fig = px.bar(transfers_received_by_member_association, x=transfers_received_by_member_association.index, y=transfers_received_by_member_association.values, \
+                     labels={'x':'Member Association', 'y':'Amount (USD)'},\
+                     title="FEES RECEIVED BY MEMBER ASSOCIATION (SUMMER 2024)", width=1000, height=600)
+        #change the color of the bars to green if the value
+        #is positive and red if the value is negative
+        fig.update_traces(marker_color=['green' if x >= 0 else \
+                    'red' for x in transfers_received_by_member_association.values], \
+        marker_line_color='rgb(8,48,107)', marker_line_width=1.5,\
+              opacity=0.6, selector=dict({'type':'bar'}))
+        #change the size of the bars to 0.5
+        fig.update_layout(bargap=0.5)
+        #change the background color to white
+        fig.update_layout(plot_bgcolor='white')
+        #change the font color to black
+        fig.update_layout(font_color='black')
+        #change the title font size to 20
+        fig.update_layout(title_font_size=20)
+        #change the title font color to black
+        fig.update_layout(title_font_color='black')
+        #change the xaxis font size to 15
+        fig.update_layout(xaxis_tickfont_size=15)
+        #change the yaxis font size to 15
+        fig.update_layout(yaxis_tickfont_size=15)
+        #change the xaxis title font size to 20
+        fig.update_layout(xaxis_title_font_size=20)
+        #change the yaxis title font size to 20
+        fig.update_layout(yaxis_title_font_size=20)
+        #change the xaxis title font color to black
+        fig.update_layout(xaxis_title_font_color='black')
+        #change the yaxis title font color to black
+        fig.update_layout(yaxis_title_font_color='black')
+        #change the xaxis tick angle to 45 degrees
+        fig.update_layout(xaxis_tickangle=45)
+        #add annotation in bottom right position
+        #font smaller. "Source: FIFA"
+        fig.add_annotation(text="Source: FIFA", xref="paper", \
+        yref="paper", x=1, y=-0.15, showarrow=False, font=dict(size=12))
+        #show the graph
+        st.plotly_chart(fig, use_container_width=True)
        
 #add annotation in bottom right position
 #font smaller. "Source: FIFA"
@@ -186,3 +233,6 @@ if show_graph:
         yref="paper", x=1, y=-0.15, showarrow=False, font=dict(size=12))
 #show the graph
         st.plotly_chart(fig, use_container_width=True)
+
+#if the show_members is clicked, show Member Assocation graphs
+
